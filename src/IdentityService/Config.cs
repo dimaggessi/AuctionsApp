6a112c297cf93f670a6a63d633ac8788ac7b565a
dimaggessi <diegosmteixeira@gmail.com>
2024-07-05 12:59:19 -0300

    add: remaining contracts and consumers

--- src/IdentityService/Config.cs
+++ src/IdentityService/Config.cs
@@ -29,6 +29,23 @@ public static class Config
 				RedirectUris = {"https://www.getpostman.com/oauth2/callback"},
 				ClientSecrets = new[] {new Secret("NotASecret".Sha256())},
 				AllowedGrantTypes = {GrantType.ResourceOwnerPassword}
+			},
+			new Client 
+			{
+				// the name nextApp is used only for a browser based web application
+				ClientId = "nextApp",
+				ClientName = "nextApp",
+				ClientSecrets = {new Secret("secret".Sha256())},
+				// do not send the secret to client's browser
+				// it's happening behind the scenes (access token returned from identity server)
+				AllowedGrantTypes = GrantTypes.CodeAndClientCredentials,
+				// if using a mobile app, it's not possible store a secret inside a Reactive Native app
+				RequirePkce = false, // it's not needed for this case
+				RedirectUris = {"http://localhost:3000/api/auth/callback/id-server"},
+				AllowOfflineAccess = true,
+				// specify the scopes that's client is allowed to access
+				AllowedScopes = {"openid", "profile", "auctionApp"},
+				AccessTokenLifetime = 3600*24*30 // (one month lifetime) only for developing purpose
 			}
 		};
 }
