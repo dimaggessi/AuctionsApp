dad27b3488684e73c10ceaea8b671e6bb53a3b60
dimaggessi <diegosmteixeira@gmail.com>
2024-07-30 19:51:26 -0300

    test: testing setup for integration tests

--- src/AuctionService/Program.cs
+++ src/AuctionService/Program.cs
@@ -5,81 +5,77 @@ using MassTransit;
 using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.EntityFrameworkCore;
 
-internal class Program
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddControllers();
+builder.Services.AddDbContext<AuctionDbContext>(options =>
 {
-	private static void Main(string[] args)
+	options.UseNpgsql(builder.Configuration.GetConnectionString("DefaultConnection"));
+});
+builder.Services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
+builder.Services.AddMassTransit(x =>
+{
+	// MassTransit.EntityFrameworkCore package
+	// Add an Outbox to avoid data inconsistency when Message Bus is down
+	x.AddEntityFrameworkOutbox<AuctionDbContext>(o =>
 	{
-		var builder = WebApplication.CreateBuilder(args);
+		// it looks the outbox every 10 seconds and try to deliver the message
+		o.QueryDelay = TimeSpan.FromSeconds(10);
 
-		// Add services to the container.
-		builder.Services.AddControllers();
-		builder.Services.AddDbContext<AuctionDbContext>(options =>
+		// MassTransit doesn't have a SQL Server option
+		o.UsePostgres();
+		o.UseBusOutbox();
+	});
+
+	// Add Fault Consumer from Search Service
+	x.AddConsumersFromNamespaceContaining<AuctionCreatedFaultConsumer>();
+
+	// Set Exchange endpoint to "auction-auction-created-fault"
+	x.SetEndpointNameFormatter(new KebabCaseEndpointNameFormatter("auction", false));
+
+	x.UsingRabbitMq((context, cfg) =>
+	{
+		cfg.Host(builder.Configuration["RabbitMq:Host"], "/", host =>
 		{
-			options.UseNpgsql(builder.Configuration.GetConnectionString("DefaultConnection"));
-		});
-		builder.Services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
-		builder.Services.AddMassTransit(x =>
-		{
-			// MassTransit.EntityFrameworkCore package
-			// Add an Outbox to avoid data inconsistency when Message Bus is down
-			x.AddEntityFrameworkOutbox<AuctionDbContext>(o =>
-			{
-				// it looks the outbox every 10 seconds and try to deliver the message
-				o.QueryDelay = TimeSpan.FromSeconds(10);
-
-				// MassTransit doesn't have a SQL Server option
-				o.UsePostgres();
-				o.UseBusOutbox();
-			});
-
-			// Add Fault Consumer from Search Service
-			x.AddConsumersFromNamespaceContaining<AuctionCreatedFaultConsumer>();
-
-			// Set Exchange endpoint to "auction-auction-created-fault"
-			x.SetEndpointNameFormatter(new KebabCaseEndpointNameFormatter("auction", false));
-
-			x.UsingRabbitMq((context, cfg) =>
-			{
-				cfg.Host(builder.Configuration["RabbitMq:Host"], "/", host => 
-				{
-					host.Username(builder.Configuration.GetValue("RabbitMq:Username", "guest"));
-					host.Password(builder.Configuration.GetValue("RabbitMq:Password", "guest"));
-				} );
-				
-				cfg.ConfigureEndpoints(context);
-			});
+			host.Username(builder.Configuration.GetValue("RabbitMq:Username", "guest"));
+			host.Password(builder.Configuration.GetValue("RabbitMq:Password", "guest"));
 		});
 
-		builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
-			.AddJwtBearer(options =>
-			{
-				options.Authority = builder.Configuration["IdentityServiceUrl"];
-				options.RequireHttpsMetadata = false;
-				options.TokenValidationParameters.ValidateAudience = false;
-				options.TokenValidationParameters.NameClaimType = "username";
-			});
-		
-		builder.Services.AddScoped<IAuctionRepository, AuctionRepository>();
-		
-		builder.Services.AddGrpc();
+		cfg.ConfigureEndpoints(context);
+	});
+});
 
-		var app = builder.Build();
+builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+	.AddJwtBearer(options =>
+	{
+		options.Authority = builder.Configuration["IdentityServiceUrl"];
+		options.RequireHttpsMetadata = false;
+		options.TokenValidationParameters.ValidateAudience = false;
+		options.TokenValidationParameters.NameClaimType = "username";
+	});
 
-		// Configure the HTTP request pipeline.
-		app.UseAuthentication();
-		app.UseAuthorization();
-		app.MapControllers();
-		app.MapGrpcService<GrpcAuctionService>();
+builder.Services.AddScoped<IAuctionRepository, AuctionRepository>();
 
-		try
-		{
-			DbInitializer.DbInit(app);
-		}
-		catch (Exception ex)
-		{
-			Console.WriteLine(ex.Message);
-		}
+builder.Services.AddGrpc();
 
-		app.Run();
-	}
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+app.UseAuthentication();
+app.UseAuthorization();
+app.MapControllers();
+app.MapGrpcService<GrpcAuctionService>();
+
+try
+{
+	DbInitializer.DbInit(app);
 }
+catch (Exception ex)
+{
+	Console.WriteLine(ex.Message);
+}
+
+app.Run();
+public partial class Program { }
---
+++ tests/AuctionService.IntegrationTests/Fixtures/CustomWebAppFactory.cs
@@ -0,0 +1,44 @@
+using AuctionService.Data;
+using MassTransit;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc.Testing;
+using Microsoft.AspNetCore.TestHost;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using Testcontainers.PostgreSql;
+using WebMotions.Fake.Authentication.JwtBearer;
+
+namespace AuctionService.IntegrationTests;
+
+public class CustomWebAppFactory : WebApplicationFactory<Program>, IAsyncLifetime
+{
+	private readonly PostgreSqlContainer _postgreSqlContainer = new PostgreSqlBuilder().Build();
+
+	public async Task InitializeAsync()
+	{
+		// inside docker this is going to start a running instance of a test container database server
+		await _postgreSqlContainer.StartAsync();
+	}
+
+	protected override void ConfigureWebHost(IWebHostBuilder builder)
+	{
+		// everything loaded from the Program.cs will be replaced inside here
+		builder.ConfigureTestServices(services =>
+		{
+			services.RemoveDbContext<AuctionDbContext>();
+			
+			services.AddDbContext<AuctionDbContext>(options =>
+			{
+				options.UseNpgsql(_postgreSqlContainer.GetConnectionString());
+			});
+
+			// MassTransit provides it
+			// it's remove the configuration inside Program.cs and replace it with a Test Container
+			services.AddMassTransitTestHarness();
+			
+			services.EnsureCreated<AuctionDbContext>();
+		});
+	}
+
+	Task IAsyncLifetime.DisposeAsync() => _postgreSqlContainer.DisposeAsync().AsTask();
+}
